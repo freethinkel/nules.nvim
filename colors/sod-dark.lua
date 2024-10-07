4e948231cbb2052b3343fa0b526e0e2d776107f4
Egor Kolesnikov <freethinkel@gmail.com>
2024-10-07 14:18:28 +0700

    init

---
+++ colors/sod-dark.lua
@@ -0,0 +1,27 @@
+require("themes.highlights").setup({
+	schema = {
+		base00 = "#1B1B1B",
+		base01 = "#262626",
+		base02 = "#3C3C3C",
+		base03 = "#3C3C3C",
+		base04 = "#5f5f5f",
+		base05 = "#A0A0A0",
+		base06 = "#E9E9E9",
+		base07 = "#A9F0FD",
+		base09 = "#FFFFFF",
+		base08 = "#FFFFFF", -- just foreground
+		base0A = "#A0A0A0",
+		base0B = "#404141",
+		base0C = "#A0A0A0",
+		base0D = "#A0A0A0",
+		base0E = "#A0A0A0",
+		base0F = "#FFFFFF",
+	},
+	colors = {
+		red = "#FA7583",
+		green = "#9FECB7",
+		blue = "#9BCAFF",
+		yellow = "#E1C08D",
+		accent = "#ffffff",
+	},
+})
